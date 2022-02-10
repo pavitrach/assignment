ad2449c2f94e963379d8c7afbaae64ba85a33aa5
pavitrach <99277567+pavitrach@users.noreply.github.com>
2022-02-10 21:49:24 +0530

    Add files via upload

---
+++ student result.py
@@ -0,0 +1,14 @@
+sno=int(input("enter student number"))
+sname=input("enter student name")
+sgroup=input("enter student group")
+s1=int(input("maths marks:"))
+s2=int(input("physics marks:"))
+s3=int(input("computer marks:"))
+s4=int(input("telugu marks:"))
+s5=int(input("english marks:"))
+s6=int(input("analytical marks:"))
+s7=int(input("environment marks:"))
+if s1>=35 and s2>=35 and s3>=35 and s4>=35 and s5>=35 and s6>=35 and s7>=35 :
+       print("pass")
+else :
+    print("fail")
